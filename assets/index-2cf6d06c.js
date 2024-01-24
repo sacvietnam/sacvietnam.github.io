1fcfb360a832a7144a23410c61c650808ef845ba
Nguyen Thanh Canh <thanhcanh.dev@gmail.com>
2024-01-24 15:47:35 +0700

    Updates

--- assets/index-2cf6d06c.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-d07d2743.js";import{m as e}from"./useLang-d2a68ea2.js";const m=({label:r,onPress:s})=>o.jsx(e.div,{className:"inline-block drop-shadow-md",children:o.jsx(e.button,{whileHover:{scale:1.05},whileTap:{scale:.9},onClick:s,className:"px-4 py-2 text-white rounded-md drop-shadow-md bg-primary min-w-[120px]",children:r})});export{m as T};
